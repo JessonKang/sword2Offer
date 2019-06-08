d55a28c47d920e839f5044e29ebe58408ea3a077
JessonKang <573150132@qq.com>
2019-06-08 10:37:36 +0800

    求二叉树的镜像

---
+++ MirrorTree.java
@@ -0,0 +1,48 @@
+package sword2;
+
+/*题目：求二叉树的镜像，即把所有的左右子树进行交换
+ * 二叉树的镜像定义：源二叉树 
+    	    8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+    	
+    	镜像二叉树
+    	    8
+    	   /  \
+    	  10   6
+    	 / \  / \
+    	11 9 7  5
+ * 
+ * 思路： 
+ * 		递归交换根节点的左右子树，就像交换两个整数需要一个中间变量，二叉树的左右节点交换也是如此，见下代码。
+ * */
+class TreeNode{
+	int val=0;
+	TreeNode left = null;
+	TreeNode right = null;
+	TreeNode(int va){
+		val = va;
+	}
+}
+
+public class MirrorTree {
+	public void mirror(TreeNode tree) {
+		//树为空
+		if(tree == null)
+			return ;
+		
+		//注意这里是把整个左右子树进行了交换
+		TreeNode tmp = tree.left;
+		tree.left = tree.right;
+		tree.right = tmp;
+		
+		//再对交换后的左子树和右子树进行镜像操作
+		if(tree.left != null)
+			mirror(tree.left);
+		if(tree.right != null)
+			mirror(tree.right);
+		
+	}
+}
